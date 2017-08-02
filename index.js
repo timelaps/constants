7c38731e8c45b9b724dec57e3a3c85def78df30e
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-02 18:13:40 -0500

    added init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    MAX_SAFE_INTEGER: 9007199254740991,
+    MAX_ARRAY_INDEX: Math.pow(2, 53) - 1,
+    MAX_INTEGER: Number.MAX_INTEGER,
+    BASE_TYPES: {
+        true: true,
+        false: false,
+        null: null,
+        undefined: undefined,
+        NaN: NaN
+    }
+};
